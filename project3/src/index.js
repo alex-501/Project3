2ef51e7779cd812ef27f9687e7e633ec41e23309
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-17 13:46:09 -0700

    Update index.js

--- project3/src/index.js
+++ project3/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//////////////////////////////////////////////////
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import Router from './router'
+import reducers from './reducers'
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+        <Router />
+    </BrowserRouter>
+    </Provider>
+, document.getElementById('root'));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +29,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//  more: https://create-react-app.dev/docs/measuring-performance/
 reportWebVitals();
